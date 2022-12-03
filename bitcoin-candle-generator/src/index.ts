bee8c19c333563d4767c8c65f7d39f4f80cfb812
Rhuan-Rodrigues1 <gitrhuan34@gmail.com>
2022-12-03 11:04:42 -0300

    request/datas

---
+++ bitcoin-candle-generator/src/index.ts
@@ -0,0 +1,40 @@
+import { config } from "dotenv";
+import { InternalError } from "./utils/errors/InternalError";
+import axios, {AxiosError} from "axios";
+
+config()
+
+export class ClientRequestError extends InternalError {
+    constructor(message: string) {
+      const internalMessage =
+        'Unexpected error when trying to communicate to coingecko';
+      super(`${internalMessage}: ${message}`);
+    }
+  }
+
+const readPrice = async (): Promise<number> => {
+    try {
+        const result = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
+        const data = result.data
+        const price = data.bitcoin.usd
+        return price
+
+    } catch (err) {
+        const axiosError = err as AxiosError;
+      if (
+        axiosError instanceof Error &&
+        axiosError.response &&
+        axiosError.response.status
+      ) {
+        throw new ClientRequestError(
+          `Error: ${JSON.stringify(axiosError.response.data)} Code: ${
+            axiosError.response.status
+          }`
+        );
+      }
+      throw new ClientRequestError((err as { message: any }).message);
+    }
+}
+
+
+readPrice()
